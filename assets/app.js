f62381e7aa031f72b42d720f4e0e60b4fb8338cc
asawo <asawo@mercari.com>
2019-11-25 17:47:22 +0900

    added HTTP request class, can now extract API key from env and insert into header of GET request

--- assets/app.js
+++ assets/app.js
@@ -6,16 +6,31 @@
 // 5. Map the response into a graph with chart.js
 
 // 1. Extract API key from local file for dev - not working
-import env from "/env.js";
-const localKey = JSON.stringify(env, null, " ");
-console.log(localKey);
-// const env = require('./env.js) [nope]
+import env from "../env.js";
 
 // 3. Make a get request to RESAS API
-const http = new HTTPRequest(),
-  prefectures = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
+class HTTPRequest {
+  // make a http get request
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        headers: {
+          "X-API-KEY": env.X_API_KEY
+        }
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+}
 
+//
+const http = new HTTPRequest(),
+  prefectureList = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
+
+// get prefecture list
 http
-  .get(prefectures)
+  .get(prefectureList)
   .then(data => console.log(data))
   .catch(err => console.log(err));
