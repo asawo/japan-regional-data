cba1129df9426b37e07edee597efc47c3f9ccdc0
asawo <asawo@mercari.com>
2019-12-08 15:24:40 +0900

    added chart.js with fake data to check it's working

--- assets/app.js
+++ assets/app.js
@@ -30,6 +30,26 @@ function loadDropdown(data) {
   return `<select>${prefectures}</select>`;
 }
 
+// Fake graph data
+const ctx = document.getElementById("myChart").getContext("2d");
+const chart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [0, 10, 5, 2, 20, 30, 45]
+      }
+    ]
+  },
+
+  // Configuration options go here
+  options: {}
+});
+
 // document.addEventListener("DOMContentLoaded", loadDropdown);
 // prefectureDropdown.addEventListener(onselect, function() {
 //   console.log(EventSource);
